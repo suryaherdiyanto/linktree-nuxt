c601bf2a8e9023302b7ff4167f445f76534ee2bf
suryaherdiyanto <suryaherdiyanto@gmail.com>
2023-10-05 09:14:50 +0800

    setup font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"]
+  modules: ["@nuxtjs/tailwindcss"],
+  app: {
+    head: {
+		link: [
+			{ rel: 'preconnect', href: 'https://fonts.googleapis.com'},
+			{ rel: 'preconnect', href: 'https://fonts.gstatic.com'},
+			{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600&display=swap' },
+		]
+	}
+  }
 })
