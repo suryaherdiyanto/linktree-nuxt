54eb5c9e77282df0ac2b1a30f026a923c91342de
suryaherdiyanto <suryaherdiyanto@gmail.com>
2023-10-25 22:10:43 +0800

    update layout

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="bg-gray-100 border-t-gray-200 border-t p-5">
-        <div class="container mx-auto flex">
+    <div class="border-t border-gray-300">
+        <div class="container mx-auto py-5 flex">
             <h4 class="text-lg  font-semibold ml-auto text-gray-800">Project By: Surya Herdiyanto</h4>
         </div>
     </div>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
     <div class="font-sans w-100 min-h-screen bg-gray-200">
         <TheHeader />
 
-        <div class="container mx-auto py-7 min-h-1/2">
+        <div class="container mx-auto py-7 min-h-screen">
             <slot />
         </div>
         <TheFooter />
