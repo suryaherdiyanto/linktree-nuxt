6d9cae857ad09baae4cc2aac2cbec12cc6f5ef22
suryaherdiyanto <suryaherdiyanto@gmail.com>
2023-11-05 15:49:29 +0800

    add nuxtui & nav brand color

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="border-b-gray-200 border-b bg-white">
         <div class="container mx-auto p-5">
-            <h1 class="text-xl font-bold">Link Tree App</h1>
+            <h1 class="text-xl font-bold text-gray-800">Link Tree App</h1>
         </div>
     </div>
 </template>
